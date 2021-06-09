37af514d515370386694d5e6d9be07687abaa859
Anne L'Hôte <anne.lhote@gmail.com>
2021-06-09 14:53:39 +0200

    [feat][route][#2] Add new type "all" for "pubmed" endpoint
    that will chain harvesting and parsing

--- pubmed/server/main/tasks.py
+++ pubmed/server/main/tasks.py
@@ -1,5 +1,8 @@
+from pubmed.server.main.logger import get_logger
 from pubmed.server.main.pubmed_harvest import download_one_entrez_date
 from pubmed.server.main.pubmed_parse import pubmed_to_json, pubmed_to_mongo
+
+logger = get_logger()
 
 
 def create_task_pubmed(args: dict) -> None:
@@ -11,3 +14,8 @@ def create_task_pubmed(args: dict) -> None:
         pubmed_to_json(date=date)
     elif task_type == 'load':
         pubmed_to_mongo()
+    elif task_type == 'all':
+        download_one_entrez_date(date=date)
+        pubmed_to_json(date=date)
+    else:
+        logger.error('Type error: your request should have a type between "harvest", "parse", "load" or "all".')
