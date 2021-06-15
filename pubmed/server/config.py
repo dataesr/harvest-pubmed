d42d87da603ad095ace52a0ff649920dec29b0a2
eric <eric.jeangirard@recherche.gouv.fr>
2021-06-15 09:57:57 +0200

    [fix] queue name

--- pubmed/server/config.py
+++ pubmed/server/config.py
@@ -7,7 +7,7 @@ class BaseConfig(object):
     """Base configuration."""
     WTF_CSRF_ENABLED = True
     REDIS_URL = 'redis://redis:6379/0'
-    QUEUES = ['bso-publications']
+    QUEUES = ['pubmed']
 
 
 class DevelopmentConfig(BaseConfig):
